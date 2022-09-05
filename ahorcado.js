0211e4985535aa539810da5534e5488a7466f4fa
JulioLaz <julioalazarte@gmail.com>
2022-09-05 12:08:36 -0300

    funcionando correctamente

--- ahorcado.js
+++ ahorcado.js
@@ -14,10 +14,10 @@ const palabras=[
 //ejecuta la funcion del boton comprobar con ENTER o SPACE
 let elInput = document.getElementById('letra');
 elInput.addEventListener('keyup', function(e) {
-  var keycode = e.keyCode || e.which;
-  if (keycode == 13 || keycode == 32) {
-   btnComprobar();
-  }
+      var keycode = e.keyCode;
+      if (keycode == 13 || keycode == 32) {
+         btnComprobar();
+      }
 });
 
 //cursor en input entrada
@@ -25,84 +25,92 @@ const btnEnfocar = document.querySelector("#comprobar"),
 	   $nombre = document.querySelector("#letra");
       btnEnfocar.addEventListener("click", () => {$nombre.focus();});
 
-let salida = document.querySelector(".letraIngresada"),
-    entrada = document.querySelector(".letra"),
+let entrada = document.querySelector(".letra"),
     info = document.querySelector(".info");
-
-info.value="Inserte una letra"
-//Palabra secreta aleatoria:
+    
+    //Palabra secreta aleatoria:
 let palabraSecreta=palabras[Math.round(Math.random()*10)];
-console.log(palabraSecreta)
+console.log(palabraSecreta);
+info.value="Inserte una letra: "+palabraSecreta;//en el input #info
+   
 //array sin comas
-let arrayPalabra= palabraSecreta.split("");
-//conversion de la palabra en guiones
-let palabraOculta=arrayPalabra.map((letra,indice, arrayPalabra)=>{
-    return letra==arrayPalabra[indice] ? "_ " : letra})
+let arrayPalabra= palabraSecreta.split('');
 
-let e=0;
+//conversion de la palabra en guiones
+let palabraOculta=arrayPalabra.map((l,i, arrayPalabra)=>{return l==arrayPalabra[i] ? "_ " : l});
+
+function verPalabra(palabraOculta){
+      let elem='';
+      let letras = palabraSecreta.split('');
+
+      letras.forEach((e)=> {
+         if(palabraOculta.includes(e)){
+            elem += `<div class="hidden">${e}</div>`;
+         }
+         else{
+            elem += `<div class="hidden">🙈</div>`;
+            }
+         });
+      document.getElementById("letraI").innerHTML=elem;
+   }
 
 function btnComprobar(){
-   const letra = document.querySelector("#letra").value;
-   let ltr=letra.toLowerCase()
-   
-   e++;
-   for(let i in arrayPalabra){
-      if(ltr==arrayPalabra[i]){
-         palabraOculta[i]=ltr;
-         salida.value= palabraOculta.join("");
-         console.log("palabra oculta: "+ palabraOculta)
-         entrada.value="";
+      let letra = document.querySelector("#letra").value,
+          ltr=letra.toLowerCase();
+      for(let i in arrayPalabra){
+         if(ltr==arrayPalabra[i]){
+            palabraOculta[i]=ltr;
+            entrada.value="";
+         }
       }
-   }
-   contadorFallos(ltr);
-   contadorIntentos(ltr,e);
+      contadorFallos(ltr);
+      contadorIntentos(ltr);
+      verPalabra(palabraOculta);
 }
 
-let a=0;
-let arrayErrores=[];
+let arrayErrores=[],
+    arrayEntrada=[],
+    a=0;//incrementador array de letras erradas
 
 function contadorFallos(ltr){
+   if(!((ltr=== " ")||(ltr=== ""))){//elimino espacios vacios
+      if(!arrayPalabra.includes(ltr)){
+         arrayEntrada[a]=ltr;
 
-   if(!((ltr=== " ")||(ltr=== ""))){
+         let arrayErrores = arrayEntrada.filter((item,index)=>{
+             return arrayEntrada.indexOf(item) === index});
+         let veces=3-arrayErrores.length;
+         a=a+1;
 
-   if(!arrayPalabra.includes(ltr)){
-      arrayErrores[a]=ltr;
-      a=a+1;
-      let veces=3-parseInt(a);
-      info.value= "erraste: quedan "+ veces + ' intentos';
-      if(veces===2){
-         caja(true);
+         document.getElementById("errores").innerHTML=arrayErrores;
+         info.value= "erraste: quedan "+ veces + ' intentos';
+         
+         if(veces===2){
+            caja(true);
+         }
+         if(veces===1){
+            persona();
+         }
+         entrada.value="";
+         if(arrayErrores.length === 3){
+            caja(false);
+             info.value="PERDISTE";
+             setTimeout(function(){alert("Inicia un nuevo juego");
+               location. reload()}, 500);
+         }
       }
-      if(veces===1){
-         persona();
-      }
+      }else{
       entrada.value="";
-      arrayErroresLimpio = arrayErrores.filter(Boolean);
-console.log("array sin guioen: "+ arrayErroresLimpio)
-      if(arrayErrores.length === 3){
-         caja(false);
-          info.value="PERDISTE";
-          setTimeout(function(){
-            alert("Inicia un nuevo juego");
-            location. reload();
-          }, 500);
       }
    }
-}else{
-   entrada.value="";
-}
-}
-function contadorIntentos(ltr,e){
-      
-   e=e-1;
+   
+function contadorIntentos(ltr){
    let a1=palabraOculta.join(''),
        a2=arrayPalabra.join('');
-         
    const comparacionArrayEntradaSalida = function (a1, a2){
       let i = a1.length;
       if (i != a2.length) return false;
-         
-      while (i--) {
+      while(i--){
         if (a1[i] !== a2[i]) return false;
       }
       return true;
@@ -121,9 +129,7 @@ function contadorIntentos(ltr,e){
       }
 }
 
-let guiones = palabraOculta.toString();
-
 function ponerGuiones(){
-   salida.value =palabraOculta.join("");
+   verPalabra(palabraOculta);
 }
 ponerGuiones();
