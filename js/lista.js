b50b4060ce84e60db1958ec3dd6d73f64129fd97
JulioLaz <julioalazarte@gmail.com>
2022-09-16 14:33:04 -0300

    limpiando detalles

--- js/lista.js
+++ js/lista.js
@@ -1,4 +1,3 @@
-// const listas=['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre','mango', 'manzana', 'maracuya', 'melocoton', 'membrillo', 'naranja', 'mandarina', 'nispero', 'nuez', 'oliva', 'papaya', 'pera', 'pomelo','uno','dos','tres','cuatro','cinco','seis','siete','ocho','nueve','diez','once','doce'];
 const listasMeses=['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
 const listasFrutas=['mango', 'manzana', 'maracuya', 'melocoton', 'membrillo', 'naranja', 'mandarina', 'nispero', 'nuez', 'oliva', 'papaya', 'pera', 'pomelo'];
 const listasNumeros=['uno','dos','tres','cuatro','cinco','seis','siete','ocho','nueve','diez','once','doce','trece','catorce','quince'];
