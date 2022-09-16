6e2c47559ec9845236d6fc2e2da3ddb2a6e42eef
JulioLaz <julioalazarte@gmail.com>
2022-09-15 23:37:55 -0300

    completo

--- js/ahorcado.js
+++ js/ahorcado.js
@@ -1,20 +1,13 @@
-// cursor en input entrada
-// const btnEnfocar = document.querySelector('#comprobar'),
-//       $nombre = document.querySelector('#letra');
-//       btnEnfocar.addEventListener('click', () => {$nombre.focus();});
-
 let soloLetras = /^[a-zA-Z]/;
 let entrada = document.querySelector('.letra'),
     info = document.querySelector('.info'),
     vidas = document.querySelector('#vida');
-   //  help = document.querySelector('#helpWord');
-    
-    info.value='Ingresar una Letra!!!';//en el input #info
+    help = document.querySelector('#helpWord');
+    document.getElementById('helpWord').innerHTML='NUMEROS😘';
     document.getElementById('errores').innerHTML='🤩: ';
+    info.value='Ingresar una Letra!!!';//en el input #info
     
 function listasAdd(lsts){
-   console.log('inicio0001');
-
       let numeroAleatorio=Math.floor(Math.random()*lsts.length);
       let palabraSecreta=lsts[numeroAleatorio];
      palabrita= palabraSecreta.toLowerCase();
@@ -24,74 +17,46 @@ function listasAdd(lsts){
       helpWordAdd(palabraSecreta)
       return palabraSecreta;
    }
-   function btnhelpWord(){
-   helpWordAdd(palabraSecreta)
-
+function btnhelpWord(){
+   document.querySelector("#helpWord").style.width = "20vw";
+   document.querySelector("#helpWord").style.height = "4.3vh";
+   document.querySelector("#helpWord").style.border = "1px solid black";
+   document.querySelector("#helpWord").style.background = "white";
 }
+
 function helpWordAdd(palabraSecreta){
-console.log(palabraSecreta);
-if (listasNumeros.includes(palabraSecreta)){
-   document.getElementById('helpWord').innerHTML='NUMEROS😘';
-   console.log(palabraSecreta);
-}
-   if (listasFrutas.includes(palabraSecreta)){
-      document.getElementById('helpWord').innerHTML='FRUTAS😒';
-      console.log(palabraSecreta);
-}
-   if (listasMeses.includes(palabraSecreta)){
-      document.getElementById('helpWord').innerHTML='MESES😕';
-      console.log(palabraSecreta);
+   if (listasNumeros.includes(palabraSecreta)){help.value='NUMEROS'}
+   if (listasFrutas.includes(palabraSecreta)){help.value='FRUTAS'}
+   if (listasMeses.includes(palabraSecreta)){help.value='MESES'}
 }
 
-
-}
 function arrayWord(palabraSecreta){
-   console.log('inicio0002');
-
       arrayPalabra= palabraSecreta.split('');
-      console.log('arrayWord: '+ arrayPalabra);
       wordHidden(arrayPalabra,palabraSecreta)
       return arrayPalabra;
-   }
+}
 
 function wordHidden(arrayPalabra,palabraSecreta){
-   console.log('inicio0003');
-
       palabraOculta=arrayPalabra.map((l,i, arrayPalabra)=>{return l==arrayPalabra[i] ? '_ ' : l});
       console.log('palabraOculta:  '+palabraOculta);
-      // jugar();
       verPalabra(palabraSecreta);
-      // verPalabra();
       return palabraOculta;
 }
     
 function btnPalabraAdd(){
-   console.log('inicio0004');
-
    let palabraAdd = document.querySelector('#palabraAdd');
-
       listas.push(palabraAdd.value);
       listaSinVacios= listas.filter((item) => item !== '')
-      console.log('listas add: '+ listaSinVacios);
-      console.log('listas add.lengt: '+ listaSinVacios.length);
-      console.log('palabra nueva: '+ palabraAdd);
-      document.getElementById('errores').innerHTML='🤩: ';
+      document.getElementById('errores').innerHTML='😁';
       palabraAdd.value="";
       listasAdd(listaSinVacios);
       ingresoCaracter();
       vidas.value=5;
       vida=5;
-   
 }
 //verificacion de caracteres
 function ingresoPalabra(){
-
-   console.log('inicio0005');
-   
    let inputPalabra = document.getElementById('palabraAdd');
-   // let nuevoInputPalabra=inputPalabra;
-   // inputPalabra.value.toLowerCase();
-   // console.log(nuevoInputPalabra.value)
    let arrayInput=[];
    inputPalabra.addEventListener('keyup', e=> {
    dato = e.key;
@@ -112,10 +77,7 @@ function ingresoPalabra(){
 }
 ingresoPalabra();
 
-// ejecuta la funcion del boton comprobar con ENTER
 function ingresoCaracter(){
-   console.log('inicio0006');
-
          let elInput = document.getElementById('letra');
          elInput.addEventListener('keyup', e=> {
          dato = e.key;
@@ -133,14 +95,10 @@ function ingresoCaracter(){
 }
 
 function verPalabra(palabraSecreta){
-   console.log('inicio0007: palabraOculta: '+palabraOculta);
-   console.log('inicio0007: palabraSecreta: '+palabraSecreta);
-   // console.log('inicio0004: arrayPalabra: '+arrayPalabra.join(''));
-   // console.log('inicio0004: palabraOculta'+palabraOculta.split(''));
-newArrayPalabra=arrayPalabra.join('');
+   newArrayPalabra=arrayPalabra.join('');
    if(palabraSecreta==undefined){
    palabraSecreta=newArrayPalabra.toString();
-}
+   }
       let elemento='',
           letras = palabraSecreta.split('');
 
@@ -153,22 +111,15 @@ newArrayPalabra=arrayPalabra.join('');
          }
       });
       document.getElementById('letraI').innerHTML=elemento;
-   }
+}
    
 function btnComprobar(palabraSecreta){
-   console.log('inicio0008: btnComprobar');
-   // console.log(' palabraSecreta: '+palabraSecreta);
-
-   // console.log('arrayPalabra: '+arrayPalabra);
-   // console.log('palabraOculta: '+palabraOculta);
-
       let letra = document.querySelector('#letra').value,
-         // palabraSecreta,
           ltr;
 
       if(letra.match(soloLetras)){
          ltr=letra.toLowerCase();
-         
+        
          for(let i in arrayPalabra){
             if(ltr==arrayPalabra[i]&&ltr.match(soloLetras)&&!((ltr=== ' ')||(ltr=== ''))){
                palabraOculta[i]=ltr;
@@ -187,8 +138,6 @@ let arrayErrores=[],arrayEntrada=[],vida=5;
     a=0;//incrementador array de letras erradas
 
 function contadorFallos(ltr){
-   console.log('inicio0009: contarFallos');
-
    if(!(ltr==undefined)){
       console.log(arrayPalabra);
       if(!arrayPalabra.includes(ltr)){
@@ -196,12 +145,16 @@ function contadorFallos(ltr){
 
          let arrayErrores = arrayEntrada.filter((item,index)=>{
              return arrayEntrada.indexOf(item) === index}),
+             vidasGit=[],
              veces=vida-arrayErrores.length;
              a++;
 
          document.getElementById('errores').innerHTML='😫: '+arrayErrores;
          info.value= 'Te quedan '+ veces + ' intentos';
-         vidas.value=veces
+         for(let i=1;i<=veces;i++){
+            vidasGit.push('😊');
+         }
+         vidas.value=vidasGit.join('')
          if(veces===(vida-1)){
             caja(true);
          }
@@ -216,14 +169,14 @@ function contadorFallos(ltr){
          }
          entrada.value='';
          if(arrayErrores.length === vida){
+            vidas.value="💀"
             personaDeath();
             caja(false);
-             info.value='PERDISTE';
+             info.value='JUEGA OTRA VEZ - PALABRA OCULTA: '+arrayPalabra.join('');
              document.getElementById('errores').innerHTML= "<img src=img/pinguinotriste.gif>";
              setTimeout(function(){
-             alert('!!!PERDISTE!!!'+arrayPalabra);
+             alert('!!!PERDISTE!!!  PALABRA OCULTA: '+arrayPalabra.join(''));
              arrayErrores=[],arrayEntrada=[],vida=5;
-             info.value='JUEGA OTRA VEZ';
              canvas.width=canvas.width;
              patibulo();
              btnJugar();
@@ -237,13 +190,10 @@ function contadorFallos(ltr){
 
 
 function contadorIntentos(ltr){
-   console.log('inicio00010');
-
    let a1=palabraOculta.join(''),
    a2=arrayPalabra.join('');
    
    const comparacionArrayEntradaSalida = function (a1, a2){
-
       let i = a1.length;
       if (i != a2.length) return false;
       while(i--){
@@ -275,18 +225,17 @@ function contadorIntentos(ltr){
       }
 }
 
-
-
 function btnJugar(){
-   console.log('inicio00012');
    btnPalabraAdd()
-   console.log('inicio00011');
    ingresoCaracter();
-   vidas.value=5;
+   vidas.value='😊😊😊😊😊';
    vida=5;
    info.value='JUGAR OTRA VEZ';
-
+   document.querySelector("#helpWord").style.width = "0vw";
+   document.querySelector("#helpWord").style.height = "0vh";
+   document.querySelector("#helpWord").style.border = "none";
+   document.querySelector("#helpWord").style.background = "none";
 }
 btnJugar();
 
-function refrescar(){window.location.reload()}
+function btnRefrescar(){window.location.reload()}
